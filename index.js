f82acd9f7a095374da78ee811ab514f96276c980
Darryn TSOUNGHAT <darrynchris@gmail.com>
2024-01-26 03:05:09 -0800

    modification 3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).json("Bonjour tout nous avons reliés le git à jenkins");
+  res.status(200).json("Jenkins réagit bien");
 });
 
 module.exports = app.listen(process.env.PORT || 4000, () =>
